7521b9102e9b39b9e69a129005b14135ce88e228
Sirius-Blaack <teezcree@yahoo.com>
2023-02-16 11:39:37 -0800

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,8 @@
  * Return: Always 0
  */
 int main(void)
-
 {
-	puts("\"Programming is like building a multi
-lingual puzzle");
+	printf("with proper grammar, but the outcome
+is a piece of art,\n");
 	return (0);
 }
