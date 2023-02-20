b031ace218edf0efe897093bc98e46ab252021eb
Sirius-Blaack <teezcree@yahoo.com>
2023-02-20 14:49:47 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,17 +6,15 @@
 
 int main(void)
 {
-	int  n;
-	int m;
+	char c;
 
-	for (n = 48; n <= 57; n++)
-	{
-                 putchar(n);
-	}
-	for (m = 97; m <= 102; m++)
-	{
-	         putchar(m);
-	}
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
 	putchar('\n');
 	return (0);
 }
+
